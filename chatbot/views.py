44f7d5ec26e8c57a61b76bd0bc11902421ff624f
dwk601 <108056780+dwk601@users.noreply.github.com>
2023-03-06 16:20:49 -0700

    Add Wikipedia API integration to Django project
    Implemented Wikipedia API integration in the Django project by adding a new function to the views.py file. The function uses the requests library to send an API request to the Wikipedia API and retrieve information based on a user's search query. Updated the index.html file to display the API response in the chatbot log when a user submits a question. Tested the functionality and it's working as expected.

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,7 +1,36 @@
+import requests
 from django.shortcuts import render
-
-# Create your views here.
 
 
 def my_view(request):
+    # check if the form was submitted
+    if request.method == 'POST':
+        # get the user's question from the form
+        question = request.POST['question']
+
+        # set up the parameters for the API request
+        params = {
+            "action": "query",
+            "format": "json",
+            "prop": "extracts",
+            "titles": question,
+            "exsentences": 3,
+            "explaintext": True,
+        }
+
+        # make the API request
+        response = requests.get(
+            "https://en.wikipedia.org/w/api.php", params=params)
+
+        # get the page content from the API response
+        page_content = response.json()["query"]["pages"]
+
+        # extract the page's extract
+        for page_id in page_content:
+            extract = page_content[page_id]["extract"]
+
+        # pass the extract to the template
+        return render(request, 'index.html', {'extract': extract})
+
+    # if the form was not submitted, just render the template
     return render(request, 'index.html')
