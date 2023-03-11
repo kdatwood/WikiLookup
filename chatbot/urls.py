beb256a63b35d0e16357460f78fa5f8f931974e4
dwk601 <108056780+dwk601@users.noreply.github.com>
2023-03-10 23:09:36 -0700

    urls update

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', views.chatbot_view),
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
 ]
